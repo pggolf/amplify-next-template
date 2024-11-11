bc162c0a234668e22181944b8d885d42a5100200
Morgan Douglas <pggolf@pggolf.org>
2024-11-10 22:06:30 -0800

    reverted changes

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,20 +2,12 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
 import { storage } from './storage/resource';
-import { transcriptionFunction } from './transcription-function/resource';
-import { EventType } from 'aws-cdk-lib/aws-s3';
-import { LambdaDestination } from 'aws-cdk-lib/aws-s3-notifications';
+import { sayHello } from './functions/say-hello/resource';
 
-const backend = defineBackend({
+defineBackend({
   auth,
   data,
   storage,
-  transcriptionFunction,
+  sayHello,
 });
 
-// Add S3 trigger for the transcription function
-backend.storage.resources.bucket.addEventNotification(
-  EventType.OBJECT_CREATED_PUT,
-  new LambdaDestination(backend.transcriptionFunction.resources.lambda),
-  { prefix: 'recordings/' }
-);
